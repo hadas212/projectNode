290bb1bbc951caedb678d8db387df86caac29bab
talmid2 <talmid2@byta.local>
2025-01-19 18:36:37 +0200

    change

--- models/purchase.js
+++ models/purchase.js
@@ -1,6 +1,6 @@
 import { Schema, model, Types } from "mongoose";
 
-
+//purchaseSchema
 const purchaseSchema = Schema({
     userId: { type: Types.ObjectId, ref: "user" },
     date: { type: Date, default: new Date() },
