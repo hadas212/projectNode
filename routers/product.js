755a3d53af8267b21ea24c3dc17a0851403ce8d8
riki <r0527147555@gmail.com>
2025-02-23 18:03:14 +0200

    finnal2

--- routers/product.js
+++ routers/product.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { add,updateById,deleteById,getByCategories,getAllproducts } from "../controllers/product.js";
+import { add,updateById,deleteById,getByCategories,getAllproducts,getTotalProductPages } from "../controllers/product.js";
 
 const router = Router();
 router.get("/", getAllproducts)
@@ -7,5 +7,6 @@ router.get("/:categories", getByCategories)
 router.delete("/:id", deleteById)
 router.put("/:id", updateById)
 router.post("/", add)
+router.get("/total",getTotalProductPages);
 
 export default router;
