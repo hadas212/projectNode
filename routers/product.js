a2e08feca1967a69102db6a3af498a2288325bbf
riki <r0527147555@gmail.com>
2025-02-23 19:20:53 +0200

    finnal3

--- routers/product.js
+++ routers/product.js
@@ -3,10 +3,11 @@ import { add,updateById,deleteById,getByCategories,getAllproducts,getTotalProduc
 
 const router = Router();
 router.get("/", getAllproducts)
+router.get("/total",getTotalProductPages);
 router.get("/:categories", getByCategories)
 router.delete("/:id", deleteById)
 router.put("/:id", updateById)
 router.post("/", add)
-router.get("/total",getTotalProductPages);
+
 
 export default router;
